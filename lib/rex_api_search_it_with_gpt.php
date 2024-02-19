c4df4d4031281d5969d4ef16c992918ea4bfd15b
Alexander Walther <github.com@alexplus.de>
2024-02-19 16:19:03 +0100

    fix oooops

--- lib/rex_api_search_it_with_gpt.php
+++ lib/rex_api_search_it_with_gpt.php
@@ -64,16 +64,10 @@ class rex_api_search_it_with_gpt extends rex_api_function
                 $articleContent = new rex_article_content($article->getId());
                 $content = $articleContent->getArticle(1); // 1 ist die ID des Slices, den Sie abrufen möchten
 
-                $hit_server = $server;
-                if (rex_addon::get('yrewrite')->isAvailable()) {
-                    $hit_domain = rex_yrewrite::getDomainByArticleId($url_sql->getValue('article_id'), $url_sql->getValue('clang_id'));
-                    $hit_server = rtrim($hit_domain->getUrl(), '/');
-                }
-
                 if ($article instanceof rex_article) {
                     $formattedResults[] = [
                         'title' => $article->getTitle(),
-                        'url' => $hit_server . rex_getUrl($hit['fid'], $hit['clang']),
+                        'url' => $server . rex_getUrl($hit['fid'], $hit['clang']),
                         'teaser' => $hit['highlightedtext'],
                         'content' => $content,
                     ];
@@ -88,10 +82,7 @@ class rex_api_search_it_with_gpt extends rex_api_function
                 $url_sql->setWhere(['url_hash' => $hit['fid']]);
                 if ($url_sql->select('article_id, clang_id, profile_id, data_id, seo')) {
                     if ($url_sql->getRows() > 0) {
-                        $url_info = json_decode($url_sql->getValue('seo'), true);
-                        $url_profile = Url\Profile::get($url_sql->getValue('profile_id'));
 
-                        // get yrewrite article domain
                         $hit_server = $server;
                         if (rex_addon::get('yrewrite')->isAvailable()) {
                             $hit_domain = rex_yrewrite::getDomainByArticleId($url_sql->getValue('article_id'), $url_sql->getValue('clang_id'));
